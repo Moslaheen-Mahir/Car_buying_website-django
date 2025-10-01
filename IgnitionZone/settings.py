60075d62696e9d866f05c7a3f5dec412337eb166
Moslaheen-Mahir <121347117+Moslaheen-Mahir@users.noreply.github.com>
2025-10-01 09:56:42 +0600

    Update settings.py

--- IgnitionZone/settings.py
+++ IgnitionZone/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',
